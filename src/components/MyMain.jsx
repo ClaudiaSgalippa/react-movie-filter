3122a346580dcf2e462b8fdca96da20e1e9231ea
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-01 17:43:02 +0200

    Pre-impostazione useEffect

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -1,10 +1,20 @@
+import {useState, useEffect} from "react"; {/*Creazione della variabile di stato*/}
+import MyFilms from "./MyFilms"; {/*Importiamo l'array di MyFilms.jsx nel MyMain.jsx*/}
+
 const MyMain = () => {
+
+    const [films, setFilms] = useState(MyFilms);  {/*Funzione di useState hook per gestire i cambiamenti dei dati*/}
+    const [selectGenre, setSelectGenre] = useState (""); {/*Funzione di useState hook per gestire i cambiamenti dei dati*/}
+
+    useEffect (() => {
+        
+    })
 
     return (
         <>
             <header>
                 <div className="onlyMain">
-                    <h1>Main</h1>
+                    <select></select>
                 </div>
             </header>
         </>
